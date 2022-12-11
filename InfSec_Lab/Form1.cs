513dc45a29d569b6a949b3cfa229f634501cdfd6
AginS <agins.main@gmail.com>
2022-12-11 16:55:29 +0300

    added write and read files

--- InfSec_Lab/Form1.cs
+++ InfSec_Lab/Form1.cs
@@ -7,19 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
 
 namespace InfSec_Lab
 {
     public partial class Form1 : Form
     {
+
+        List<UserJSON> Users = new List<UserJSON>();
+
         public Form1()
         {
             InitializeComponent();
+
+            string curFile = "users.txt";
+            if (!File.Exists(curFile))
+            {
+                UserJSON admin = new UserJSON("ADMIN");
+                AllUsersJson users = new AllUsersJson(admin);
+                string json = JsonSerializer.Serialize(users);
+                Console.WriteLine("Cannot found users.json. Creating...");
+                using (StreamWriter writer = new StreamWriter(curFile, false))
+                {
+                    writer.WriteLineAsync(json);
+                }
+                Users.Add(admin);
+            } else
+            {
+                Console.WriteLine("users.json founded");
+                using (StreamReader reader = new StreamReader(curFile))
+                {
+                    string json = reader.ReadToEnd();
+                    Console.WriteLine(json);
+                    AllUsersJson? restoredAllUsers = JsonSerializer.Deserialize<AllUsersJson>(json);
+                    Users = restoredAllUsers.UserData;
+                }
+            }
         }
 
         private void AuthButton_Click(object sender, EventArgs e)
         {
-
+            string curFile = "users.txt";
+            if (!File.Exists(curFile))
+            {
+                UserJSON admin = new UserJSON("ADMIN");
+                AllUsersJson users = new AllUsersJson(admin);
+                string json = JsonSerializer.Serialize(users);
+                Console.WriteLine(json);
+                using (StreamWriter writer = new StreamWriter(curFile, false))
+                {
+                    writer.WriteLineAsync(json);
+                }
+            } else
+            {
+                Console.WriteLine("Exist");
+            }
         }
     }
 }
---
+++ InfSec_Lab/UserJSON.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace InfSec_Lab
+{
+    public class UserJSON
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public UserJSON(string login, string password)
+        {
+            Login = login;
+            Password = password;
+        }
+
+        public UserJSON(string login)
+        {
+            Login = login;
+            Password = "";
+        }
+    }
+
+    public class AllUsersJson
+    {
+        public List<UserJSON> UserData { get; set; }
+
+        public AllUsersJson(UserJSON userJSON)
+        {
+            UserData = new List<UserJSON>();
+            UserData.Add(userJSON);
+        }
+
+        public AllUsersJson(List<UserJSON> usersJSON)
+        {
+            UserData = usersJSON;
+        }
+    }
+}
