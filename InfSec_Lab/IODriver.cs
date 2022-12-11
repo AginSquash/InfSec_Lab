c6456473cf3a681e6f18f70bd774d6d1be05c391
AginS <agins.main@gmail.com>
2022-12-11 20:24:44 +0300

    Created IO Driver

---
+++ InfSec_Lab/IODriver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace InfSec_Lab
+{
+    class IODriver
+    {
+        static string curFile = "users.json";
+        static public void WriteUsersData(List<UserJSON> users)
+        {
+            AllUsersJson allUsersJson = new AllUsersJson(users);
+            string json = JsonConvert.SerializeObject(users);
+            using (StreamWriter writer = new StreamWriter(curFile, false))
+            {
+                writer.WriteLineAsync(json);
+            }
+        }
+
+        static public List<UserJSON> ReadUsersData()
+        {
+            using (StreamReader reader = new StreamReader(curFile))
+            {
+                string json = reader.ReadToEnd();
+                Console.WriteLine(json);
+                AllUsersJson restoredAllUsers = JsonConvert.DeserializeObject<AllUsersJson>(json);
+                List<UserJSON> Users = restoredAllUsers.UsersData;
+                return Users;
+            }
+        }
+    }
+}
