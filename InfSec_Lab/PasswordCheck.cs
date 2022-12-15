432bd27d546b414e62c5fc5c2e6421790d54ce5a
AginS <agins.main@gmail.com>
2022-12-16 00:47:45 +0300

    Добавил проверку пароля по регсам

---
+++ InfSec_Lab/PasswordCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace InfSec_Lab
+{
+    class PasswordCheck
+    {
+        public static bool isOK(String pass)
+        {
+            string pattern1 = @"[-.?!)(,:]";
+            string pattern2 = @"[a-zA-Z]";
+
+            if (Regex.IsMatch(pass, pattern1) && Regex.IsMatch(pass, pattern2)) {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
